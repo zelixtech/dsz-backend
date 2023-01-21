f7e98c3b14cdd485e6a2c9497ecddbc6776a686b
Aayush Jain <imjainaayush@gmail.com>
2023-01-21 16:15:42 +0530

    removing extra code and fixed issues

--- src/routes/invoice.js
+++ src/routes/invoice.js
@@ -3,21 +3,8 @@ const express = require('express')
 const router = express.Router()
 
 const { invoiceController } = require('../controllers/')
-const {
-  allowEmployee,
-  allowAdmin,
-  allowHR,
-  allowBothAdminAndHR,
-} = require('../middlewares')
+const { allowEmployee } = require('../middlewares')
 
 router.post('/createInvoice', allowEmployee, invoiceController.createInvoice)
-
-// router.get('/:invoice_id', allowEmployee, invoiceController.retrieveQuotation)
-
-// router.get(
-//   '/all/:invoice_id',
-//   allowEmployee,
-//   invoiceController.retrieveQuotationsOfQuery
-// )
 
 module.exports = router
