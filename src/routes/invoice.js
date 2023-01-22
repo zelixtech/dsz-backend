1fb53897c80cc637fae1369476122055d61b1456
Aayush Jain <imjainaayush@gmail.com>
2023-01-22 22:56:59 +0530

    added api endpoint

--- src/routes/invoice.js
+++ src/routes/invoice.js
@@ -5,6 +5,12 @@ const router = express.Router()
 const { invoiceController } = require('../controllers/')
 const { allowEmployee } = require('../middlewares')
 
-router.post('/createInvoice', allowEmployee, invoiceController.createInvoice)
+router.post('/:client_id', allowEmployee, invoiceController.createInvoice)
+router.get('/:invoice_id', allowEmployee, invoiceController.retrieveInvoice)
+router.get(
+  '/all/:client_id',
+  allowEmployee,
+  invoiceController.retrieveInvoicesOfClient
+)
 
 module.exports = router
